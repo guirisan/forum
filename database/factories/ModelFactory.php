22775cfa254d0393f3aa50e3da95bd8d6af7504f
guirisan <guirisan@upv.es>
2017-05-15 12:30:58 +0200

    lesson 3

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -33,6 +33,7 @@ $factory->define(App\Thread::class, function (Faker\Generator $faker) {
         'body' => $faker->paragraph
     ];
 });
+
 $factory->define(App\Reply::class, function (Faker\Generator $faker) {
     return [
         'user_id' => function(){
