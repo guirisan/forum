00c0a4284341829f84051be9439db4bf60187eb9
guirisan <guirisan@upv.es>
2017-05-15 12:05:39 +0200

    lesson 2

---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+
+    /** @test */
+    public function a_user_can_see_all_threads()
+    {
+        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads');
+
+        $response->assertSee($thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_browse_individual_thread()
+    {
+        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads/'. $thread->id);
+
+        $response->assertSee($thread->body);
+    }
+
+
+
+
+}
