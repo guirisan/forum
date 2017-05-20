67ead51901898747bfad60d2994329961482dce1
guirisan <guirisan@upv.es>
2017-05-20 15:46:23 +0200

    lesson 5

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -21,12 +21,32 @@
             </div>
         </div>
     </div>
+
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             @foreach ($thread->replies as $reply)
-                @include('threads._reply')
+                @include('threads.reply')
             @endforeach
         </div>
     </div>
+
+    @if (auth()->check())
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <textarea name="body" id="body" class="form-control" rows="5" required="required" placeholder="Comment something..."></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Comment</button>
+            </form>
+        </div>
+    </div>
+    @else
+        <p class="text-center">
+            Please <a href="{{ route('login') }}">sign in</a> to participate
+        </p>
+    @endif
+
 </div>
 @endsection
