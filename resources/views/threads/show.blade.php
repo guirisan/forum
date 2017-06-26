cbcf02f6d64ae9552b2ac801ab4266dd334159e3
guirisan <guirisan@upv.es>
2017-06-26 10:42:34 +0200

    lesson 15

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 ">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <a href="#">
@@ -19,20 +19,12 @@
                     {{ $thread->body }}
                 </div>
             </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+            
             @foreach ($thread->replies as $reply)
                 @include('threads.reply')
             @endforeach
-        </div>
-    </div>
 
-    @if (auth()->check())
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+            @if (auth()->check())
             <form method="POST" action="{{ $thread->path() . '/replies' }}">
                 {{ csrf_field() }}
                 <div class="form-group">
@@ -40,13 +32,23 @@
                 </div>
                 <button type="submit" class="btn btn-primary">Comment</button>
             </form>
+            @else
+                <p class="text-center">
+                    Please <a href="{{ route('login') }}">sign in</a> to participate
+                </p>
+            @endif
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    Thread published {{ $thread->created_at->diffForHumans() }} by 
+                    <a href="#">{{ $thread->owner->name}}</a>, and has {{$thread->replies->count() }} replies.
+                </div>
+            </div>
+
         </div>
     </div>
-    @else
-        <p class="text-center">
-            Please <a href="{{ route('login') }}">sign in</a> to participate
-        </p>
-    @endif
+
 
 </div>
 @endsection
