e9ae588980d2d9bd619a1d766e4f0b8519ee5ec7
guirisan <guirisan@upv.es>
2017-07-08 13:07:32 +0200

    lesson 30

---
+++ resources/views/profiles/activities/created_favorite.blade.php
@@ -0,0 +1,22 @@
+@component('profiles.activities.activity')
+    @slot('heading')    
+
+        <a href="{{ $activity->subject->favorited->path() }}">
+            {{ $profileUser->name }} favorited a reply.
+        </a>
+
+{{--         <a href='{{ $activity->subject->thread->path() }}'>
+            {{ $activity->subject->thread->title }}
+        </a>
+        <span>
+            posted 
+            {{ $activity->subject->created_at->diffForHumans() }}
+        </span>
+    
+ --}}    @endslot
+    
+    @slot('body')    
+        {{ $activity->subject->favorited->body }}
+    @endslot
+@endcomponent
+
