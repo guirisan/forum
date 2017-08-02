c042017ec8b1e5f62aa58a3c1c3db0ece7863230
guirisan <guirisan@upv.es>
2017-08-02 15:28:59 +0200

    lesson 40

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -35,7 +35,7 @@ class ReadThreadsTest extends TestCase
     }
 
     // /** @test */
-    // public function a_user_can_read_replies_associated_to_single_thread()
+    // function a_user_can_read_replies_associated_to_single_thread()
     // {
     //     $reply = create('App\Reply', ['thread_id' => $this->thread->id]);
 
@@ -110,5 +110,45 @@ class ReadThreadsTest extends TestCase
 
         $this->assertCount(2, $response['data']);
         $this->assertEquals(2, $response['total']);
+    }
+
+    /** @test */
+    public function a_user_can_subscribe_to_thread()
+    {
+        // given a thread
+        $thread = create('App\Thread');
+
+        // when the user subscribes
+        $thread->subscribe($userId = 1);
+
+        // we should be able to filter threads that user has subscribed
+        $this->assertEquals(
+            1,
+            $thread->subscriptions()->where('user_id', $userId)->count()
+        );
+    }
+
+    /** @test */
+    public function a_user_can_unsubscribe_from_thread()
+    {
+        // given a thread
+        $thread = create('App\Thread');
+
+        // when the user subscribes
+        $thread->subscribe($userId = 1);
+        $thread->subscribe($otherUserId = 2);
+
+        // we should be able to filter threads that user has subscribed
+        $this->assertEquals(
+            1,
+            $thread->subscriptions()->where('user_id', $userId)->count()
+        );
+
+        $thread->unsubscribe($userId);
+
+        $this->assertCount(
+            1,
+            $thread->subscriptions
+        );
     }
 }
